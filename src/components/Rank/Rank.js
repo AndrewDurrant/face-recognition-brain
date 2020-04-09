482047224a2990393a46c89f878721b02090fbb4
Andrew Durrant <andrew.durrant75@gmail.com>
2020-04-09 11:53:16 -0600

    initial commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Rank.css';
+
+const Rank = () => {
+  return (
+    <section>
+      <div className='white f3'>
+        {'Andrew, your current rank is...'}
+      </div>
+      <div className='white f1'>
+        {'#7'}
+      </div>
+    </section>
+  )
+}
+
+export default Rank;
